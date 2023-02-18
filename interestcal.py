d644502993e9b95e9df38fc22b9fbed0474aa90c
lukepop <lukepopovic@gmail.com>
2023-02-18 12:24:19 +1100

    created interestcal.py

---
+++ interestcal.py
@@ -0,0 +1,18 @@
+#Question 3  
+# REV HISTORY
+#~~~~~~~~~~~~~~~~~~~~~
+# A - 20230217
+# 
+#~~~~~~~~~~~~~~~~~~~~~
+# take user input 
+i = 0 
+while i == 0: 
+years = int(input("Years invested:")) 
+
+if years != :
+#    print('Please input whole years only')
+#elif years == 0:
+ #   i = 1
+#else:
+ #   i = 1
+
