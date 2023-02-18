02f1d99edf778aae72c71e5c69f3c73b339d413f
lukepop <lukepopovic@gmail.com>
2023-02-18 12:58:53 +1100

    interestcal.py modified
    finished and checked

--- interestcal.py
+++ interestcal.py
@@ -4,15 +4,30 @@
 # A - 20230217
 # 
 #~~~~~~~~~~~~~~~~~~~~~
-# take user input 
+# take user input of integers only, including 0 and only support whole numbers years (integers) 
+# 
+#  input initial deposit amount 
+deposit = float(input("Deposit amount ($): "))
+
+#  input initial deposit amount 
+interest = float(input("Interest rate (%): "))
+
+# check on years, if a non integer is parsed then the code loops until the while condition i !=0 is met 
 i = 0 
 while i == 0: 
-years = int(input("Years invested:")) 
+    try:
+        years = int(input("Years invested:")) 
+        i = 1
+    except: 
+        i = 0 
 
-if years != :
-#    print('Please input whole years only')
-#elif years == 0:
- #   i = 1
-#else:
- #   i = 1
-
+#create a list to loop through for n number of years 
+n = 0 
+while n <= years: 
+    if n == 0: 
+        balance = deposit 
+        n+=1
+    else: 
+       balance = balance+balance*(interest/100)
+       n+=1   
+print(f"Balance after {years} years: ${balance}")
